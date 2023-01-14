a757e425f824b3939fc6b5ed783d6c6e504108e6
Putt <57494734+PuttTim@users.noreply.github.com>
2023-01-15 03:45:45 +0800

    ✨ Added banner

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
 import React from "react"
-import { Box } from "@chakra-ui/react"
+import { Box, Center, Heading } from "@chakra-ui/react"
 import { Route, Routes } from "react-router-dom"
 import Calculator from "./pages/Calculator"
 
 const App = () => {
     return (
-        <Box maxW="1500px" m="auto" px="16px">
-            <Routes>
-                <Route path="/" element={<Calculator />} />
-            </Routes>
-        </Box>
+        <>
+            <Box bg="customGrey.500" w="auto" p="8px" mb="24px">
+                <Center>
+                    <Heading>GPA buy wat?</Heading>
+                </Center>
+            </Box>
+            <Box maxW="1500px" m="auto" px="16px">
+                <Routes>
+                    <Route path="/" element={<Calculator />} />
+                </Routes>
+            </Box>
+        </>
     )
 }
 
