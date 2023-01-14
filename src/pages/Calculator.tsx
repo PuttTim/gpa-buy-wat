b84dcfb36035dde7301272e21cccc16e708e058b
Nitish <79391172+TechSupportz@users.noreply.github.com>
2023-01-14 15:13:45 +0800

    ✨ initial setup done

---
+++ src/pages/Calculator.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Calculator = () => {
+    return (
+        <>
+            <div>Calculator</div>
+            <div className="font-body">Calculator</div>
+        </>
+    )
+}
+
+export default Calculator
