fdb3f8ebabcf8732ceca6d9a13f69f73c23d56cc
PuttTim <yannaputt@gmail.com>
2023-04-05 07:44:59 +0800

    ⚡ Made ResultText to be inline-block

--- src/components/ResultText.tsx
+++ src/components/ResultText.tsx
@@ -8,13 +8,18 @@ interface ResultTextProps {
 
 export const ResultText = (props: ResultTextProps) => {
     return (
-        <HStack align="center">
-            <Heading>{props.header}</Heading>
-            <Text fontSize="3xl" fontWeight="medium">
-                {props.header === "Your GPA: "
-                    ? parseFloat(props.text).toFixed(2)
-                    : props.text}
+        <>
+            <Text fontSize="3xl" fontWeight="bold">
+                {props.header}{" "}
+                <span>
+                    {" "}
+                    <Text display="inline-block" fontWeight={300}>
+                        {props.header === "Your GPA: "
+                            ? parseFloat(props.text).toFixed(2)
+                            : props.text}
+                    </Text>
+                </span>
             </Text>
-        </HStack>
+        </>
     )
 }
