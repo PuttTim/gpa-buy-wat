57055692e09511b648d6ba72253def0857b55d2e
Nitish <79391172+TechSupportz@users.noreply.github.com>
2023-04-05 21:54:32 +0800

    🐞 Vertically centered footer text

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -14,92 +14,81 @@ import { GitHub } from "react-feather"
 
 const Footer = () => {
     return (
-        <Box
+        <VStack
             h="100%"
             minH="10vh"
             position="sticky"
             bgColor="customGrey.500"
             w="100%"
             px={8}
-            py={4}>
-            <VStack>
-                <Heading
-                    textAlign="center"
-                    size={{
-                        base: "sm",
-                        md: "md",
-                    }}>
-                    <span>
-                        <Tooltip label="Project was originally made for NUS Hackers' Hack&Roll 2023!">
-                            <Heading
-                                size={{
-                                    base: "sm",
-                                    md: "md",
-                                }}
-                                display="inline-block"
-                                textDecoration="dotted">
-                                Project
-                            </Heading>
-                        </Tooltip>
-                    </span>{" "}
-                    made with ❤️ by{" "}
-                    <span>
-                        <a href="https://github.com/PuttTim" target="_blank">
-                            <Text
-                                display="inline-block"
-                                textDecoration="underline">
-                                PuttTim
-                            </Text>
-                        </a>
-                    </span>
-                    {", "}
-                    <span>
-                        <a
-                            href="https://github.com/TechSupportz"
-                            target="_blank">
-                            <Text
-                                display="inline-block"
-                                textDecoration="underline">
-                                TechSupportz
-                            </Text>
-                        </a>
-                    </span>
-                    {", "}
-                    <span>
-                        <a href="https://github.com/Coeeter/" target="_blank">
-                            <Text
-                                display="inline-block"
-                                textDecoration="underline">
-                                Coeeter
-                            </Text>
-                        </a>
-                    </span>
-                </Heading>
-                <a
-                    href="https://github.com/PuttTim/gpa-buy-wat"
-                    target="_blank">
-                    <HStack>
-                        <Icon>
-                            <GitHub />
-                        </Icon>
-                        <Text
-                            fontSize={{
+            py={4}
+            justifyContent="center">
+            <Heading
+                textAlign="center"
+                size={{
+                    base: "sm",
+                    md: "md",
+                }}>
+                <span>
+                    <Tooltip label="Project was originally made for NUS Hackers' Hack&Roll 2023!">
+                        <Heading
+                            size={{
                                 base: "sm",
                                 md: "md",
-                            }}>
-                            Project repository on{" "}
-                            <span>
-                                <Text
-                                    display="inline-block"
-                                    textDecoration="underline">
-                                    GitHub
-                                </Text>
-                            </span>
+                            }}
+                            display="inline-block"
+                            textDecoration="dotted">
+                            Project
+                        </Heading>
+                    </Tooltip>
+                </span>{" "}
+                made with ❤️ by{" "}
+                <span>
+                    <a href="https://github.com/PuttTim" target="_blank">
+                        <Text display="inline-block" textDecoration="underline">
+                            PuttTim
                         </Text>
-                    </HStack>
-                </a>
-            </VStack>
-        </Box>
+                    </a>
+                </span>
+                {", "}
+                <span>
+                    <a href="https://github.com/TechSupportz" target="_blank">
+                        <Text display="inline-block" textDecoration="underline">
+                            TechSupportz
+                        </Text>
+                    </a>
+                </span>
+                {", "}
+                <span>
+                    <a href="https://github.com/Coeeter/" target="_blank">
+                        <Text display="inline-block" textDecoration="underline">
+                            Coeeter
+                        </Text>
+                    </a>
+                </span>
+            </Heading>
+            <a href="https://github.com/PuttTim/gpa-buy-wat" target="_blank">
+                <HStack>
+                    <Icon>
+                        <GitHub />
+                    </Icon>
+                    <Text
+                        fontSize={{
+                            base: "sm",
+                            md: "md",
+                        }}>
+                        Project repository on{" "}
+                        <span>
+                            <Text
+                                display="inline-block"
+                                textDecoration="underline">
+                                GitHub
+                            </Text>
+                        </span>
+                    </Text>
+                </HStack>
+            </a>
+        </VStack>
     )
 }
 
